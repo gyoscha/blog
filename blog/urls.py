0db5f11bcb8587c244b414b5663126c1ac491671
gyoscha <sokolovgeorgy@gmail.com>
2022-07-31 15:57:45 +0300

    fix urls

--- blog/urls.py
+++ blog/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('django.contrib.auth.urls')),
+    path('users/', include('django.contrib.auth.urls')),
     path('', include('note.urls')),
     path('', include('note_api.urls')),
 ]
--- note_api/urls.py
+++ note_api/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('notes/', views.NoteAPIView.as_view()),
     path('notes/<int:pk>/', views.NoteDetailAPIView.as_view()),
-    path('users/', views.UsersAPIView.as_view(), name='users'),
-    path('users/<int:pk>/', views.UsersDetailAPIView.as_view()),
+    path('accounts/', views.UsersAPIView.as_view(), name='users'),
+    path('accounts/<int:pk>/', views.UsersDetailAPIView.as_view()),
 ]
